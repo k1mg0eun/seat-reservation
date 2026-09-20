dfc2900c8026af456e3cca126b8cbe574f4248ec
kimgoeun <kkykky0707@gmail.com>
2026-09-20 16:23:52 +0900

    fix: 예외 메시지 오타 수정

--- src/main/java/com/example/seatreservation/reservation/ReservationService.java
+++ src/main/java/com/example/seatreservation/reservation/ReservationService.java
@@ -54,7 +54,7 @@ public class ReservationService {
         Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 예약입니다."));
         reservation.cancel(userId);
 
-        Seat seat = seatRepository.findById(reservation.getSeatId()).orElseThrow(() -> new IllegalStateException("예약에 연결된 자석이 없습니다"));
+        Seat seat = seatRepository.findById(reservation.getSeatId()).orElseThrow(() -> new IllegalStateException("예약에 연결된 좌석이 없습니다."));
         seat.release();
     }
 }
